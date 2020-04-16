873efe92881e3860a2a7829ba63e9566bb6d9cb4
AnnaSun <annnasun@outlook.com>
2020-04-17 00:46:42 +0800

    阻塞队列生产者消费模式

--- src/com/company/ProdConsumer_TraditionDemo.java
+++ src/com/company/ProdConsumer_TraditionDemo.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -7,17 +8,73 @@ import java.util.concurrent.locks.ReentrantLock;
 class ShareData{
     private int num = 0;
     private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
 
     public void increment() throws Exception{
-        //判断
-        while(num!=0){
-            //等待，不能生产
+        lock.lock();
+        try {
+            //判断
+            while(num!=0){
+                //等待，不能生产
+                condition.await();
+            }
+            //加一
+            num++;
+            System.out.println(Thread.currentThread().getName()+"\t"+num);
+            //通知唤醒
+            condition.signalAll();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
         }
+
+    }
+
+    public void decrement() throws Exception{
+        lock.lock();
+        try {
+            //判断
+            while(num==0){
+                //等待，不能消费
+                condition.await();
+            }
+            //减一
+            num--;
+            System.out.println(Thread.currentThread().getName()+"\t"+num);
+            //通知唤醒
+            condition.signalAll();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+
     }
 }
 public class ProdConsumer_TraditionDemo {
 
     public static void main(String[] args){
+        ShareData shareData = new ShareData();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    shareData.increment();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"AA").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    shareData.decrement();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        },"BB").start();
 
     }
 }
